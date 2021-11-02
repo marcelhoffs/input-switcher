8d20fe9a095344785240ade973aa7c58925dd91d
Marcel Hoffs <marcelhoffs@gmail.com>
2021-11-02 17:20:53 +0100

    comment bat file

--- windows/switch_to_2.bat
+++ windows/switch_to_2.bat
@@ -1,2 +1,5 @@
+rem Switch MX Keys to channel 2
 .\hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x09,0x1e,0x01,0x00,0x00
+
+rem Switch MX Anywhere 3 to channel 2
 .\hidapitester.exe --vidpid 046D:C52B --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0a,0x1b,0x01,0x00,0x00
